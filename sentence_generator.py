ddd640634917e14c8e99e1af896cfeac29aad273
Jihee Hwang <jiheeh@stanford.edu>
2016-11-13 21:07:18 -0800

    sentences to read file successfully working

--- sentence_generator.py
+++ sentence_generator.py
@@ -1,5 +1,15 @@
+import itertools
 
+#Given a set of input words, generates sentecnes 
 
 with open('input_lexicon.txt') as f:
-    input_lexicon = f.readlines()
+    input_lexicon = f.read().splitlines()
+outfile = open('sentences_to_read.txt', 'w')
 
+sentences_to_read = itertools.permutations(input_lexicon)
+for sentence in sentences_to_read:
+
+  outfile.write("%s\n" % ' '.join(sentence))
+
+
+#print sentences_to_read
