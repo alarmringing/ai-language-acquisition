dfe2504dd6276ca8b36d27c266e3b8906277a835
khalerc <eokhale@stanford.edu>
2016-11-13 21:53:39 -0800

    ran baseline

--- baseline.py
+++ baseline.py
@@ -1,16 +1,16 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
-audiofile = AudioSegment.from_mp3("mailchimp.mp3")
-audio_chunks = split_on_silence(audiofile, min_silence_len=50, silence_thresh=-19)
+audiofile = AudioSegment.from_mp3("audio/input/self_recorded_files/ee_self_recorded.mp3")
+audio_chunks = split_on_silence(audiofile, min_silence_len=20, silence_thresh=-28)
 print len(audio_chunks)
 for i, chunk in enumerate(audio_chunks):
 	if i == 0:
 		combined_sounds = chunk
 	else:
 		combined_sounds += chunk
-	out_file = ".//splitAudio//chunk{0}.wav".format(i)
+	out_file = ".//audio//output//self_recorded_files//eechunk{0}.wav".format(i)
 	print "exporting", out_file
 	chunk.export(out_file, format="wav")
 
-combined_sounds.export(".//splitAudio/finalChunk.wav", format = "wav")
+combined_sounds.export(".//audio/output/self_recorded_files/eefinalChunk.wav", format = "wav")
