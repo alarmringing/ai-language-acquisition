33f29eff6c18fce792bdda2094aaf5412e5e0fb3
Jihee Hwang <jiheeh@stanford.edu>
2016-12-16 06:43:10 -0800

    Included ucs process in the pipeline

--- pipeline.py
+++ pipeline.py
@@ -3,15 +3,38 @@ import create_syllables
 import mfcc_clusterer
 import splice_audio
 import tuple_counter
-import word_segmenter 
+import word_segmenter  
+import dictionary_builder
+import ucs_word_segmenter
+import build_final_lexicon
 
-inputPath = 'audio/input/multisyllabic'
-inputFileName = 'multisyllabic_jihee_long'
+inputDir = 'audio/input/childrens_story'
+inputPathName = 'katie_chimney'
 
-create_syllables.create_syllables(inputPath, inputFileName)
-syllables = splice_audio.splice_audio(inputPath, inputFileName)
-labels = mfcc_clusterer.clusterAudioSegments(syllables, "audio/clustered/syllables", "multisyllabic_jihee_long_cluster", 22050, 12) #k number is arbitrary for now
-transProb = tuple_counter.tuple_counter(labels)
-#tweak constant so that word_segmenter should generate roughly 80 words
-words = word_segmenter.word_segmenter(syllables, labels, transProb, 0.025)
-wordClusters = mfcc_clusterer.clusterAudioSegments(words, "audio/clustered/words", "multisyllabic_jihee_long_cluster", 22050, 6)
+#detect syllables
+onset_times = create_syllables.create_syllables(inputDir, inputPathName)
+
+#splice audio by syllable
+syllables = splice_audio.splice_audio(inputDir, inputPathName, onset_times)
+
+#cluster syllables 
+labels, segResults = mfcc_clusterer.clusterAudioSegments(syllables, "audio/clustered/syllables", inputPathName, 22050, 12) #k number is arbitrary for now
+labelsTuple = tuple(labels.tolist())
+
+#create dictionary of costs, a.e. how often each syllable combination occurs
+cost_dictionary = dictionary_builder.dictionary_builder(labelsTuple, 4)
+
+#run ucs based on the cost dictionary above, segment audio into appropriate parts
+word_sequence = ucs_word_segmenter.segmentWord(labelsTuple, cost_dictionary, 4)
+
+# use ucs action data to re-build the final lexicon
+lexicon = build_final_lexicon.build_final_lexicon(word_sequence)
+
+#referencing final lexicon, create example audio segments using randomly sampled syllable audio from each cluster
+build_final_lexicon.generate_word_audio(segResults, 5, lexicon, 10, "audio/clustered/words/ucs", inputPathName, 22050)
+
+
+#PROCESS BEFORE UCS WAS IMPLEMENTED
+#transProb = tuple_counter.tuple_counter(labels)
+#words = word_segmenter.word_segmenter(syllables, labels, transProb, 0.01)
+#wordClusters = mfcc_clusterer.clusterAudioSegments(words, "audio/clustered/words", inputPathName, 22050, 6)
